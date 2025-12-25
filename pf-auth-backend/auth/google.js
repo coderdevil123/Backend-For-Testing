cba279d4daacf2f438e3c82ad844d599504d57c2
Shubhang Mishra <shubhangmishra094@gmail.com>
2025-12-25 21:50:58 +0530

    creating server for testing

--- pf-auth-backend/auth/google.js
+++ pf-auth-backend/auth/google.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'https://backend-for-testing-9h8v.onrender.com/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
