744b05f7ce66d14faddb2d959057fe6302ff4fd2
Shubhang Mishra <shubhangmishra094@gmail.com>
2026-01-20 18:47:34 +0530

    making new route for task in the website

--- pf-auth-backend/routes/tasks.js
+++ pf-auth-backend/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabase = require('../lib/supabase');
+const { supabase }= require('../lib/supabase');
 const auth = require('../middlewares/auth');
 
 /**
