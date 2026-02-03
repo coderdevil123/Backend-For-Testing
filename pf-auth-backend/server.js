c7e4d89cb196b9ec17372e95ea0f0ac9f7bdf48e
Shubhang Mishra <shubhangmishra094@gmail.com>
2026-02-03 19:32:18 +0530

    adding url in the backend for growthsupercharge

--- pf-auth-backend/server.js
+++ pf-auth-backend/server.js
@@ -35,7 +35,7 @@ app.use(
 const ALLOWED_ORIGINS = [
   'https://pf-workspace.vercel.app',
   'http://10.10.10.57:8090',
-  'https://pfworkspace.growthsupercharged.com/',
+  'https://pfworkspace.growthsupercharged.com',
 ];
 
 app.use(
@@ -76,7 +76,7 @@ app.get('/auth/google', (req, res, next) => {
   const ALLOWED_ORIGINS = [
     'https://pf-workspace.vercel.app',
     'http://10.10.10.57:8090',
-    'https://pfworkspace.growthsupercharged.com/',
+    'https://pfworkspace.growthsupercharged.com',
   ];
 
   if (origin && ALLOWED_ORIGINS.includes(origin)) {
