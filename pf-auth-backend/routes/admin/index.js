5d47c90e867e37d033358ce3a98518f7bedf5635
Shubhang Mishra <shubhangmishra094@gmail.com>
2026-02-10 18:52:02 +0530

    admin page

--- pf-auth-backend/routes/admin/index.js
+++ pf-auth-backend/routes/admin/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const auth = require('../middlewares/auth');
+const auth = require('../../middlewares/auth');
 const { requireAdmin } = require('../../services/adminAccess');
 
 const router = express.Router();
